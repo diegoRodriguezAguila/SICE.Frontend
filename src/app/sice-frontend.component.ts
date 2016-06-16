be10d406a58763b7130fd77e891cb5336859d6d8
diegoRodriguezAguila <diroag@gmail.com>
2016-06-16 08:49:18 -0400

    Added login, home and basic navigation

--- src/app/sice-frontend.component.ts
+++ src/app/sice-frontend.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import {LoginComponent} from './+login/login.component';
+import { HomeComponent } from './+home/home.component';
 
 @Component({
-  moduleId: module.id,
-  selector: 'sice-frontend-app',
-  templateUrl: 'sice-frontend.component.html',
-  styleUrls: ['sice-frontend.component.css']
+    moduleId: module.id,
+    selector: 'sice-frontend-app',
+    templateUrl: 'sice-frontend.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+    {path: '/', component: HomeComponent},
+    {path: '/login', component: LoginComponent},
+    {path: '/home', component: HomeComponent}
+])
 export class SiceFrontendAppComponent {
-  title = 'sice-frontend works!';
+    title = 'SICE Elfec';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { SiceFrontendAppComponent, environment } from './app/';
+import { SiceFrontendAppComponent } from './app/sice-frontend.component';
+import { environment } from './app/environment';
+//import { SiceFrontendAppComponent, environment } from './app/';
+import { ROUTER_PROVIDERS } from '@angular/router';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(SiceFrontendAppComponent);
+bootstrap(SiceFrontendAppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
 
